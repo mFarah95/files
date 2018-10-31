2c7d61fc8272016f7b7ce83bc45a5b1718cfd983
mFarah95 <tafe_5@hotmail.com>
2018-10-31 17:07:57 +0100

    task 2 done

--- nlp/NLP1.py
+++ nlp/NLP1.py
@@ -1,14 +1,58 @@
 import random
 import codecs
 import re
+import string
+import warnings
+warnings.filterwarnings(action='ignore', category=UserWarning, module='gensim')
+import gensim
+import io
+import copy
+import sys
+from nltk.stem.porter import PorterStemmer
 
+
+############################### PART 1 ##################################
 random.seed(123)
 
+# Open text
+f = io.open("nlp/pg3300.txt", 'r', encoding="utf-8")
+# Remove text punctuation and lower case
+text = f.read().translate(str.maketrans('', '', string.punctuation)).lower()
+# Split text into paragraphs
+split_paragraph = re.split(r"\n\s*\n", str(text))
 
-f = codecs.open("nlp/pg3300.txt", "r", "utf-8")
-text = f.read().encode('utf-8')
 
-t = re.split(r"\n\s*\n", text)
-#lines = f.read().split("\r\n\r\n")
-print(len(t))
-print(t[6])
+# Filter out paragraphs that contain "gutenberg"
+filtered_word = filter(
+    lambda prgh: 'gutenberg' not in prgh, split_paragraph)
+
+# Tokenize paragraphs (split them into words) and remove whitespaces
+tokenized = [prgh.split() for prgh in filtered_word]
+
+# Stemmeing
+stemmer = PorterStemmer()
+stemmed = [[stemmer.stem(word) for word in parh] for parh in tokenized]
+
+
+############################### PART 2 ##################################
+
+# Load "common english words" text file
+cm_w = io.open("nlp/common-english-words.txt", "r", encoding="utf-8")
+stopwords = cm_w.read().split(",")
+
+# Init Corpora dictionary
+dictionary = gensim.corpora.Dictionary(stemmed)
+
+# Filter stopwords that are not in dictinoary
+stopwords = list(filter(
+    lambda word: word in dictionary.token2id, stopwords))
+
+# Get stopword IDs
+stopwords_ids = list(
+    map(lambda w: dictionary.token2id[w], stopwords))
+
+# Filter stopwords
+dictionary.filter_tokens(stopwords_ids)
+
+# Map paragraphs into Bags-of-Words using the dictionary
+bow = [dictionary.doc2bow(prgh) for prgh in stemmed]
